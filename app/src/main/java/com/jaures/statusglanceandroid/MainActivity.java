9d456f21826415e5431aab3340008c04f2ef6a47
Beinjamin <jauresbeinjamin@gmail.com>
2024-05-21 12:44:20 +0100

    Declaration des varibale des composant de l interface utilisateur

--- app/src/main/java/com/jaures/statusglanceandroid/MainActivity.java
+++ app/src/main/java/com/jaures/statusglanceandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jaures.statusglanceandroid;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FloatingWidgetView floatingWidgetView;
+    private Button showDateButton;
+    private Button toggleThemeButton;
+    private boolean isDarkTheme = false; // Indique l'état actuel du thème
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
